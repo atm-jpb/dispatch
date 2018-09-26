a5eb28dd6e1503f5fb98a63feda0ef85dfdf5412
Marc de Lima Lucio <marc.delimalucio@atm-consulting.fr>
2018-09-26 15:46:12 +0200

    FIX: tabs not correctly defined at module activation

--- core/modules/moddispatch.class.php
+++ core/modules/moddispatch.class.php
@@ -120,7 +120,13 @@ class moddispatch extends DolibarrModules
 		// Example: $this->tabs = array('objecttype:+tabname1:Title1:mylangfile@mymodule:$user->rights->mymodule->read:/mymodule/mynewtab1.php?id=__ID__',  	// To add a new tab identified by code tabname1
         //                              'objecttype:+tabname2:Title2:mylangfile@mymodule:$user->rights->othermodule->read:/mymodule/mynewtab2.php?id=__ID__',  	// To add another new tab identified by code tabname2
         //                              'objecttype:-tabname':NU:conditiontoremove);                                                     						// To remove an existing tab identified by code tabname
-        
+
+		if(! defined('ATM_ASSET_NAME'))
+		{
+			define('INC_FROM_DOLIBARR', true);
+			dol_include_once('/dispatch/config.php');
+		}
+
         $this->tabs = array(
         	'delivery:+dispatch:Détail expédition:dispatch@dispatch:$conf->' . ATM_ASSET_NAME . '->enabled:/dispatch/detail.php?id=__ID__'
         	,'contract:+dispatchAsset:Détail équipement:dispatch@dispatch:$conf->' . ATM_ASSET_NAME . '->enabled:/dispatch/asset.php?id=__ID__&type_object=contrat'
