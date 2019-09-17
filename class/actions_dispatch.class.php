2f0d178b0d6908c638c335b00bcdfdca4cc1e7be
John Botella <john.botella@atm-consulting.fr>
2019-09-17 10:14:29 +0200

    Add asset/dispatch batch col

--- class/actions_dispatch.class.php
+++ class/actions_dispatch.class.php
@@ -29,16 +29,20 @@ class ActionsDispatch
 	}
 
      /** Overloading the addMoreActionsButtons function : replacing the parent's function with the one below
-      *  @param      parameters  meta datas of the hook (context, etc...) 
-      *  @param      object             the object you want to process (an invoice if you are in invoice module, a propale in propale's module, etc...) 
-      *  @param      action             current action (if set). Generally create or edit or null 
-      *  @return       void 
+      *  @param      parameters  meta datas of the hook (context, etc...)
+      *  @param      object             the object you want to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+      *  @param      action             current action (if set). Generally create or edit or null
+      *  @return       void
       */
 	function addMoreActionsButtons($parameters, &$object, &$action, $hookmanager)
 	{
 		global $conf;
 
 		$TContexts = explode(':', $parameters['context']);
+
+		// Add more cols on card lines table
+		$this->addMoreColsOnCardTable($parameters, $object, $action, $hookmanager);
+
 
 		if(in_array('ordersuppliercard', $TContexts))
 		{
@@ -86,17 +90,17 @@ class ActionsDispatch
 
 		// pour implementation dans Dolibarr 3.7
 		if (in_array('pdfgeneration',explode(':',$parameters['context']))) {
-			
+
 			define('INC_FROM_DOLIBARR',true);
 			dol_include_once('/dispatch/config.php');
 			dol_include_once('/' . ATM_ASSET_NAME . '/class/asset.class.php');
 			dol_include_once('/dispatch/class/dispatchdetail.class.php');
 			dol_include_once('/dispatch/class/dispatchasset.class.php');
 			dol_include_once('/core/lib/product.lib.php');
-			
+
 			global $conf;
 			if(! empty($parameters['object']) && (get_class($object) == 'Expedition' || get_class($object) == 'Livraison')) {
-				
+
 				$PDOdb = new TPDOdb;
 
 				$expedition = $object;
@@ -106,7 +110,7 @@ class ActionsDispatch
 				}
 
 				foreach($object->lines as &$line){
-					
+
 					$details = new TDispatchDetail;
 
 					$fkExpeditionLine = $line->id;
@@ -180,5 +184,152 @@ class ActionsDispatch
 		if(! empty($conf->global->ASSET_SHOW_DLUO) && empty($conf->global->DISPATCH_HIDE_DLUO_PDF) && ! empty($asset->date_dluo)) $desc.= ' (DLUO : '.$asset->get_date('dluo').')';
 
 		$line->desc.= $desc;
+	}
+
+
+
+	/*
+	 * Overloading the addMoreActionsButtons function
+	 *
+	 * @param   array()         $parameters     Hook metadatas (context, etc...)
+	 * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	public function addMoreColsOnCardTable(&$parameters, &$object, &$action, $hookmanager)
+	{
+		global $conf, $langs, $db;
+
+		if(intval(DOL_VERSION) < 11 && empty($conf->global->DISPATCH_SHOW_BATCH_COL_IN_CARD)){
+			return 0;
+		}
+
+		if(empty($conf->assetatm->enabled)){
+			return 0;
+		}
+
+
+		$contextArray = explode(':',$parameters['context']);
+
+		if(in_array('expeditioncard',$contextArray))
+		{
+
+			define('INC_FROM_DOLIBARR', true);
+			dol_include_once('/dispatch/config.php');
+			dol_include_once('/' . ATM_ASSET_NAME . '/class/asset.class.php');
+			dol_include_once('/dispatch/class/dispatchdetail.class.php');
+			dol_include_once('/dispatch/class/dispatchasset.class.php');
+
+			$expedition = $object;
+
+			$jsonObjectData =array();
+			foreach($object->lines as $i => $line)
+			{
+				$jsonObjectData[$line->id] = new stdClass();
+				$jsonObjectData[$line->id]->id = $line->id;
+
+				$product = new Product($db);
+				if(!empty($line->fk_product)){
+					$product->fetch($line->fk_product);
+				}
+
+				$jsonObjectData[$line->id]->batch = '';
+
+				// GET DISPATCH INFOS
+				$PDOdb = new TPDOdb;
+				$details = new TDispatchDetail();
+
+				$fkExpeditionLine = $line->id;
+
+				if(get_class($object) == 'Livraison') {
+					$fkExpeditionLine = 0;
+
+					foreach($expedition->lines as $lineExpe) {
+						if($lineExpe->fk_origin_line == $line->fk_origin_line) { // La ligne d'origine de la livraison est la ligne de commande et non la ligne d'expédition
+							$fkExpeditionLine = $lineExpe->id;
+							break;
+						}
+					}
+				}
+
+				if(! empty($fkExpeditionLine)) {
+					$TRecepDetail = $details->LoadAllBy($PDOdb, array('fk_expeditiondet' => $fkExpeditionLine));
+
+					if(count($TRecepDetail) > 0) {
+						if(!empty($line->description) && $line->description != $line->desc) $line->desc.=$line->description.'<br />'; // Sinon Dans certains cas desc écrase description
+						$batch = '';
+
+						foreach($TRecepDetail as $detail) {
+							$asset = new TAsset;
+							$asset->load($PDOdb, $detail->fk_asset);
+							$asset->load_asset_type($PDOdb);
+
+							$unite = (($asset->assetType->measuring_units == 'unit') ? 'unité(s)' : measuring_units_string($detail->weight_reel_unit, $asset->assetType->measuring_units));
+
+							if(empty($asset->lot_number)) {
+								$batch = $asset->serial_number;
+							} else {
+								$batch = $asset->lot_number." x ".$detail->weight_reel." ".$unite;
+							}
+
+							if(! empty($conf->global->ASSET_SHOW_DLUO) && empty($conf->global->DISPATCH_HIDE_DLUO_PDF) && ! empty($asset->date_dluo)) $batch.= ' (DLUO : '.$asset->get_date('dluo').')';
+							$jsonObjectData[$line->id]->batch = $batch;
+						}
+					}
+				}
+
+			}
+
+			$totalAddedCols = 1;
+			?>
+			<script>
+
+                $( document ).ready(function(){
+                    var jsonObjectData = <?php print json_encode($jsonObjectData) ; ?> ;
+
+                    // ADD NEW COLS
+                    $("#tablelines tr").each(function( index ){
+						//console.log($( this ).data( "product_type" ));
+                        if($( this ).hasClass( "liste_titre" )){
+                            // PARTIE TITRE
+                            $('<td align="center" class="linecoldispatch"><?php print $langs->transnoentities('colDispatch'); ?></td>').insertAfter($( this ).find("td:first"));
+                        }
+                        else if($( this ).data( "product_type" ) == "9"){
+                            $( this ).find("td[colspan]:first").attr('colspan',    parseInt($( this ).find("td[colspan]:first").attr('colspan')) + <?php print $totalAddedCols; ?>  );
+                        }
+                        else
+                        {
+                            // PARTIE LIGNE
+                            var nobottom = '';
+                            if($( this ).hasClass( "liste_titre_create" ) || $( this ).attr("data-element") == "extrafield" ){
+                                nobottom = ' nobottom ';
+                            }
+
+                            $('<td align="center" class="linecoldispatch' + nobottom + '"></td>').insertAfter($( this ).find("td.linecoldescription"));
+
+                            if($( this ).hasClass( "liste_titre_create" )){
+                                $( this ).find("td.linecoledit").attr('colspan',    parseInt($( this ).find("td.linecoledit").attr('colspan')) + <?php print $totalAddedCols; ?>  );
+                            }
+
+                            if($( this ).attr("data-element") == "extrafield"){
+                                $( this ).find("td[colspan]:first").attr('colspan',    parseInt($( this ).find("td[colspan]:first").attr('colspan')) + <?php print $totalAddedCols; ?>  );
+                            }
+                        }
+                    });
+
+                    // Affichage des données
+                    $.each(jsonObjectData, function(i, item) {
+                        // Dimensions
+                        $("#row-" + jsonObjectData[i].id + " .linecoldispatch:first").html(jsonObjectData[i].batch);
+
+                    });
+
+                });
+			</script>
+			<?php
+		}
+
+		return 0;
 	}
 }
